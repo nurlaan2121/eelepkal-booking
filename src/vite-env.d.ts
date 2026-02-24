c4a99de8ddfe2b93cf7ba2f482a9cd226f3b2336
Nurlan Abiibillaev <128040210+nurlaan2121@users.noreply.github.com>
2026-02-24 22:50:29 +0600

    Change VITE_API_BASE_URL and VITE_TIMEOUT types
    Updated environment variable types for API base URL and timeout.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE_URL = https://eelepkal.com
-    readonly VITE_TIMEOUT = 10000
+    readonly VITE_API_BASE_URL: string
+    readonly VITE_TIMEOUT: string
     // more env variables...
 }
+
 
 interface ImportMeta {
     readonly env: ImportMetaEnv
